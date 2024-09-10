91206b01692060c58d7625f2c8e6ec195890fa71
vincent Nam <91612042+iamvincentnam@users.noreply.github.com>
2024-09-10 02:22:22 +0100

    Add files via upload

--- techvantage.js
+++ techvantage.js
@@ -1,5 +1,12 @@
+
 gsap.from('.anim1',{ duration:1, y:-20, ease: "ease"});
-gsap.from('.hero_item', { duration:1, x:100, ease:  "power1.in"});
+
+gsap.from('.hero_item', { duration:1,opacity:0, ease:  "power1.in"});
+
+
+
+
+
 
 const middle_nav = document.querySelectorAll('.middle_nav li  a');
 middle_nav.forEach((link)=>{
@@ -12,21 +19,16 @@ middle_nav.forEach((link)=>{
 });
 
 });
-// window.onload = function() {
-//             window.scrollBy(0, 2); // Scroll 2 pixels down
-//         };
+
 const hero_item1 = document.querySelector('.hero_item1');
 const hero_item = document.querySelector('.hero_item');
 const left_hand_side = document.querySelector('.left_hand_side');
 const contact_hero_image = document.querySelector('.contact_hero_image');
 
 
-// gsap.from('.nav_anim',{opacity:0, duration:1, y:-20, ease:  "sine.inOut", stagger:.10});
-
 const hamburger_button= document.querySelector('#hamburger');
 hamburger_button.addEventListener('click', ()=>{
-  console.log( document.querySelector('.middle_nav'))
-  console.log(  document.querySelector('.login_nav'))
+ 
   document.querySelector('.middle_nav').classList.toggle('active');
   document.querySelector('.login_nav').classList.toggle('active');
   gsap.from('.active .nav_anim',{opacity:0, duration:1, y:-20, ease:  "sine.inOut", stagger:.10});
@@ -35,4 +37,43 @@ hamburger_button.addEventListener('click', ()=>{
 
 
 
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.testimonials_div',{
+  y:100,
+  opacity:0,
+  duration:.5,
+  stagger: .5,
+  scrollTrigger: {
+    trigger: '.testimonials_div', 
+    start: 'top 80%', 
+    end: 'bottom 50%', 
+    scrub:.2, 
+  
+  }
+})
+gsap.from('.card',{
+  y:50,
+  opacity:0,
+  duration:1,
+  stagger: .5,
+  scrollTrigger: {
+    trigger: '.card',
+    start:"top 80%",
+ 
+  
+  }
+})
+gsap.from('.our-clients',{
+  x:-100,
+  opacity:0,
+  duration:1,
+  stagger: .50,
+  scrollTrigger: {
+    trigger: '.our-clients',
+    start:"top 80%",
+ 
+  
+  }
+})
 
